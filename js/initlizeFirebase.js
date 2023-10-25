b9839baf3aa00d976ed4ea68140d13b425c269f8
ShaharBrandman <67471700+ShaharBrandman@users.noreply.github.com>
2023-10-25 12:12:13 +0300

    Add files via upload

---
+++ js/initlizeFirebase.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDuC842Sk78bBEmkhi1pX5WHCIvg4apVJI",
+  authDomain: "sonicsteersystems.firebaseapp.com",
+  databaseURL: "https://sonicsteersystems-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "sonicsteersystems",
+  storageBucket: "sonicsteersystems.appspot.com",
+  messagingSenderId: "985948393326",
+  appId: "1:985948393326:web:4695a96f9803acd6893b7f",
+  measurementId: "G-3JNVP2F5PW"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+console.log("Firebase is initlized and connected to the Cloud!");
