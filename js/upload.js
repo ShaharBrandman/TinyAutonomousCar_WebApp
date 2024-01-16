ef1b852b15af8bd076ba39ef38260c9d357a435b
Shahar Brandman <shaharbrandman@gmail.com>
2024-01-16 17:41:42 +0200

    upload canvas implemention

---
+++ js/upload.js
@@ -0,0 +1,14 @@
+const resHeight = 800;
+const resWidth = 600;
+
+window.addEventListener('DOMContentLoaded', () => {
+    changeHeader();
+
+    const canvas = document.getElementById('canvasContainer');
+    canvas.addEventListener('mousedown', () => {
+        return;
+    })
+    canvas.addEventListener('mouseup', () => {
+        return;
+    })
+});
